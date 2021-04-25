ce5657bcff62b05927ecba886757458926b2dd7a
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-04-25 17:23:10 -0400

    Create mdl-fix.js

---
+++ mdl-fix.js
@@ -0,0 +1,35 @@
+// Make imported models use a new path determined by BeePKG-V2
+
+
+// Copied from vtf-conv.js
+String.prototype.toBytes = function() {
+  return this.split('').map((x)=>{return x.charCodeAt()})
+}
+
+class mdl_model {
+	constructor(raw) {
+		if (raw.length < 128) {throw('MDL file too short!')}
+		// I have to manipulate data within these coordinates. If it's too short, what the fuck am I even doing???
+		this.parsed = new Uint8Array(raw)
+	}
+	
+	setPath(pathStr) {
+		function fixListLength(x,le) {return x.concat(new Array(le-x.length).fill(0))}
+		
+		let path = pathStr.toBytes()
+		if (path.length > 64) {throw('Path too long! Model will be corrupted.')}
+		if (path.length < 64) {path = fixListLength(path,64)}
+		
+		path.forEach((x,y)=>{
+			this.parsed[y+12] = x // Model path starts at 0x0000000C (byte 12)
+		})
+	}
+	
+	getPath() {
+		return (new TextDecoder().decode( this.parsed.slice(12,12+64) ))
+	}
+	
+	export() {
+		return this.parsed
+	}
+}
